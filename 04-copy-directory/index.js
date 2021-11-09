e04ebc6e139a1f3d8d193e607995ce1204ad3cdd
Pepe the Ronin <89341250+roninpepe@users.noreply.github.com>
2021-11-09 17:23:52 +0300

    fix: folder actualizing

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,7 @@ const fs = require('fs'),
 async function copyDirectory(src, dest) {
 	let srcpath = path.join(__dirname, src),
 		destpath = path.join(__dirname, dest)
+	await fs.promises.rm(destpath, {force: true, recursive: true})
 	await fs.promises.mkdir(destpath, {recursive: true})
 	try {for (const file of await fs.promises.readdir(srcpath)) {await fs.copyFile(path.join(srcpath, file), path.join(destpath, file), (err)=>{if(err) throw err})}}
 	catch(err) {console.error(err)}
